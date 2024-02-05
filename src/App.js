abf0120e995f702fb8b0da1cbd4f28f176445a67
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2024-02-06 03:00:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+
+import BotMessage from "./Components/BotMessage";
+import UserMessage from "./Components/UserMessage";
+import Messages from "./Components/Messages";
+import Input from "./Components/Input";
+
+import API from "./ChatBotAPI";
+
+import Header from "./Components/Header";
+
+function Chatbot() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function loadWelcomeMessage() {
+      setMessages([
+        <BotMessage
+          key="0"
+          fetchMessage={async () => await API.GetChatbotResponse("hi")}
+        />
+      ]);
+    }
+    loadWelcomeMessage();
+  }, []);
+
+  const send = async text => {
+    const newMessages = messages.concat(
+      <UserMessage key={messages.length + 1} text={text} />,
+      <BotMessage
+        key={messages.length + 2}
+        fetchMessage={async () => await API.GetChatbotResponse(text)}
+      />
+    );
+    setMessages(newMessages);
+  };
+
+  return (
+    <div className="chatbot">
+      <Header />
+      <Messages messages={messages} />
+      <Input onSend={send} />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chatbot />
     </div>
   );
 }
