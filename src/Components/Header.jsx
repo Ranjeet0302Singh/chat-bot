256729453f4f71da9b97e4cf447ca199d1a2dfd0
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2024-02-06 03:07:44 +0530

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="header">React Chatbot UI</div>;
+  return <div className="header">Boon Solution ChatGPT</div>;
 }
