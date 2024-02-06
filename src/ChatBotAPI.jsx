fcef4a418b72efb8b97677fec47dbf7d57f8ca85
Ranjeet Singh Rawat <ranjeet843507@gmail.com>
2024-02-06 16:32:51 +0530

    adding the response as html tags on page

--- src/ChatBotAPI.jsx
+++ src/ChatBotAPI.jsx
@@ -1,13 +1,71 @@
 const API = {
-    GetChatbotResponse: async message => {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          if (message === "hi") resolve("Welcome to RudramGPT!");
-          else resolve("echo : " + message);
-        }, 2000);
-      });
-    }
-  };
-  
-  export default API;
-  
+  GetChatbotResponse: async (message) => {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const formattedMessage = message.trim().toLowerCase();
+
+        if (formattedMessage === "hi") {
+          resolve(<p>Welcome to RudramGPT!</p>);
+        } else if (formattedMessage === "rudram solutions" ||formattedMessage === "about rudram solutions" ) {
+          resolve(
+            <div>
+              <p>
+                Transforming the landscape of data solutions into a reality.
+              </p>
+              <p style={{ textAlign: "justify" }}>
+                Welcome to Rudram Data Solutions, a pioneer in data-driven
+                solutions that aims to serve users to streamline data-related
+                problems with the adoption of advanced technologies to fuel
+                their business to the next level. We are a fleet of highly
+                qualified and skilled data scientists working together with a
+                holistic approach towards transforming raw data into actionable
+                insights for your company. At Rudram, we understand the
+                importance of data as a resource for your business and are
+                dedicated to conserve your time, money, and effort.
+              </p>
+            </div>
+          );
+        } else if (
+          formattedMessage === "what rudram solutions do?" ||  formattedMessage === "what rudram solutions do" ||
+          formattedMessage === "what are rudram solutions experties"
+        ) {
+          resolve(
+            <div>
+              <p>Rudram Solutions Expertise includes</p>
+              <ul style={{textAlign:"left"}}>
+                <li>Data Science</li>
+                <li>Machine Learning </li>
+                <li>Data Analysis </li>
+                <li>DaaS (Data as a Service)</li>
+              </ul>
+            </div>
+          );
+        } else if (
+          formattedMessage === "what services rudram solutions give?" ||
+          formattedMessage === "services of rudram solutions"
+        ) {
+          resolve(
+            <div>
+              <p>Rudram Solutions Services includes</p>
+              <ul style={{textAlign:"left"}}>
+                <li>Data Collection</li>
+                <li>Process Automation using Machine Learning and AI</li>
+                <li>Custom Data Services </li>
+                <li>Data Analytics and Reporting</li>
+                <li>Data Governance</li>
+                <li>Data Migration</li>
+                <li>Business Intelligence</li>
+                <li>Data Mining</li>
+                <li>Master Data Management(MDM)</li>
+              </ul>
+            </div>
+          );
+        } else {
+          resolve(<p> No Data Found  </p>);
+        }
+      }, 2000);
+    });
+  },
+};
+
+export default API;
